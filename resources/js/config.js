8066cbf67ad46fbad9fb387f8d02a8d8360d4f3d
detorresvc <enteng@multisyscorp.com>
2022-05-11 19:36:54 +0800

    env base config for base url

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,5 +1,6 @@
+
 export default {
-  baseUrl: 'http://localhost:8000/api',
+  baseUrl: process.env.MIX_APP_ENV === 'production' ? 'https://howtochatapp.ml/api' : 'http://localhost:8000/api',
   axiosConfig: {
     validateStatus: function (status) {
       return true
